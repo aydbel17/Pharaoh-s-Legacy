d9516b12082146b0ef693f4ea1375bb19e74ba00
aydbel17 <aydbel17@kingshigh.school.nz>
2025-07-25 12:24:54 +1200

    Bunch of things mainly pyramid door

--- pharaohs-legacy/pyramid_door.gd
+++ pharaohs-legacy/pyramid_door.gd
@@ -1,1 +1,10 @@
 extends Area2D
+@export var next_level_path: String = ""
+func _ready():
+	print("Door ready, next_level_path:", next_level_path)
+	body_entered.connect(_on_body_entered)
+func _on_body_entered(body: Node):
+	print("Collision detected with:", body.name, "Groups:", body.get_groups())
+	if body.is_in_group("Player"):
+		print("Player detected, attempting to load:", next_level_path)
+		get_tree().change_scene_to_file("res://first_level_complete.tscn")
